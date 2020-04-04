6659b30e47db3855bf7cc2ed9b918e48e11bacf7
Cratose99 <gabriel.eyer007@gmail.com>
2020-04-03 18:51:36 -0700

    fixed cdn

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 function Navbar(){
     return (
         <div>
-            <div className="row nav">
+            {/* <div className="row nav">
                 <div className="col-4 ">
                     <button className="navbarstore">store</button>
                 </div>
@@ -17,7 +17,33 @@ function Navbar(){
                 <button className="navbarawards">Awards</button>
 
                 </div>
-            </div>
+            </div> */}
+            <div class="tabbar">
+  <label class="tabbar__item">
+    <input type="radio" name="tabbar-a" checked="checked"/>
+    <button class="tabbar__button">
+      <i class="tabbar__icon ion-stop"></i>
+      <div class="tabbar__label">One</div>
+    </button>
+  </label>
+
+  <label class="tabbar__item">
+    <input type="radio" name="tabbar-a"/>
+    <button class="tabbar__button">
+      <i class="tabbar__icon ion-record"></i>
+      <div class="tabbar__label">Two</div>
+    </button>
+  </label>
+
+  <label class="tabbar__item">
+    <input type="radio" name="tabbar-a"/>
+    <button class="tabbar__button">
+      <i class="tabbar__icon ion-star"></i>
+      <div class="tabbar__label">Three</div>
+    </button>
+  </label>
+</div>
+            
         </div>
     );
     
