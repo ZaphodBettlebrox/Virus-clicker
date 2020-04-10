6802ea4764dc296a3a486825b535d645cefded51
Aidan Willett <Gamer9310@gmail.com>
2020-04-09 22:32:43 -0700

    animation path fixed

--- src/components/PageClicker/index.js
+++ src/components/PageClicker/index.js
@@ -5,28 +5,50 @@ import virus from "../../assets/Virus.png";
 import "../../hover.css";
 
 class PageClicker extends React.Component {
+
+  Clickerbobdown() {
+    const test = document.querySelector(".virus1");
+    // test.style.width=90%
+      // document.querySelector(".virus1").classList.add("virusbobdown");
+    
+  }
+
+  Clickerbobup(){
+    // document.querySelector(".virus1").classList.add("virusbobup");
+    // setTimeout(function () {
+    //   document.querySelector(".virus1").classList.remove("virusbobdown");
+    // }, 999);
+  }
+
   render() {
+
     return (
-      <div>
+      <div className="clickerPage">
         <div className="backgroundMain"></div>
         <div className="x1">
-        <img className="y1" src={minivirus} alt="virus"></img>
+          <img className="y1" src={minivirus} alt="virus"></img>
         </div>
         <div className="x2">
-        <img className="y2" src={minivirus} alt="virus"></img>
+          <img className="y2" src={minivirus} alt="virus"></img>
         </div>
         <div className="x3">
-        <img className="y3" src={minivirus} alt="virus"></img>
+          <img className="y3" src={minivirus} alt="virus"></img>
         </div>
         <div className="x4">
-        <img className="y4" src={minivirus} alt="virus"></img>
+          <img className="y4" src={minivirus} alt="virus"></img>
         </div>
         <div className="x5">
           <img className="y5" src={minivirus} alt="virus"></img>
         </div>
         <div className="backgroundTop"></div>
         <div className="clicker">
-        <img className="virus1 hvr-bounce-out hvr-bounce-out:hover hvr-bounce-out:focus hvr-bounce-out:active" src={virus} alt="virus"></img>
+          <img
+            className="virus1"
+            src={virus}
+            alt="virus"
+            onMouseDown={this.Clickerbobdown}
+            onMouseUp={this.Clickerbobup}
+          ></img>
         </div>
       </div>
     );
